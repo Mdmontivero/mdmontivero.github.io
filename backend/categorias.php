88f601a0cf5ef6d1d4fcb7de9f6f815b58093f75
Mdmontivero <117607702+Mdmontivero@users.noreply.github.com>
2022-11-07 19:03:57 -0300

    initial comit

---
+++ backend/categorias.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html >
+<head>
+    
+    <title>GARDEN</title>
+    
+</head>
+<body>
+    <header>
+        <h1 id="titulo">Blog de Maximiliano</h1>
+    </header>
+    <section>
+        <article class="post">
+            <h2>planta 1</h2>
+            <p>Jardines</p>
+            <img src="img/cannabis.jpg" width="400">
+            <p>Todo sobre la planta</p>
+        </article>    
+    
+        <article class="post">
+            <h2>planta 2</h2>
+            <p>macetas</p>
+            <img src="img/carnivora.webp" width="400">
+            <p>Todo sobre la planta</p>
+
+        </article class="post">    
+            <h2>planta 3</h2>
+            <p>perfumadas</p>
+            <img src="img/Jazmin.jpg" width="400">
+            <p>Todo sobre la planta</p>
+        </article>    
+
+
+    </section>
+    <footer>
+        <a href="#titulo">subir</a><br>
+        <a href="mailto:montivero2000@hotmail.com">Contacto </a>
+
+
+    </footer>
+    
+</body>
+</html>
