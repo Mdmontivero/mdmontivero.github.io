55506c9371f4f5b24683ef52130cf9f8d78ee94e
Mdmontivero <117607702+Mdmontivero@users.noreply.github.com>
2022-11-09 16:52:06 -0300

    Add files via upload

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -11,3 +11,38 @@ $("#from_categorias").submit(function (){
     }
    return true;
 });
+
+$("#from_productos").submit(function() {
+    var producto = $("pNombre").val();
+    var descripcion = $("#descripcion").val();
+    var categoria = $("#categoria").val ();
+    var precio = $("#precio").val ();
+    
+    var errores = [];
+    
+    if($.trim(producto)===''){
+            errores.push("debe completar el Nombre del producto");
+            
+    if($.trim(categoria)==='')
+            errores.push("debe completar la Categoria");
+            
+    if($.trim(descripcion)==='')
+            errores.push("debe completar la descripcion");
+            
+    if($.trim(precio)==='')
+            errores.push("debe completar el precio");
+            
+    if (errores.length > 0)
+        errores.push("Maximiliano Montivero");
+        alert(errores.join("\n"));
+        return false;
+    }    
+        return true;
+    
+     
+    
+    
+            
+    
+    
+    });
